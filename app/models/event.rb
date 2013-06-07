3ae2e86515c0cfb20ead6ba405c5f62fae193df5
nojoek <nojoek@me.com>
2013-06-06 23:42:47 -0500

    cleaning up some indexes

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,9 @@ class Event < ActiveRecord::Base
 
 attr_accessible :description, :final_destination, :final_time, :title, :user_id, :totalvoters
 
-has_many :locations
-has_many :event_times
-has_many :votes
+has_many :locations, :dependent => :destroy
+has_many :event_times, :dependent => :destroy
+has_many :votes, :dependent => :destroy
 belongs_to :user  ####belongs to or has many?
 
  # @events = Event.order("count DESC")
