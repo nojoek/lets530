f597dba29d56f6a0d7802aeb84495c27b463b2a1
nojoek <nojoek@me.com>
2013-06-13 18:26:47 -0400

    more mail changes and formatting fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default from: "results.letsdo@gmail.com"
   
-  def result(vote)
+  def results(vote)
       @vote = vote
       @url  = "letdo.herokuapp.com/events/#{:event_id}"
       mail(:to => vote.token, :subject => "The Results are In")
